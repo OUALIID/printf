af9df0d5a321ea06872218a5a64a559f85115780
fadwadwa <fawda1119@gmail.com>
2023-04-24 19:01:27 -0700

    test2

--- _printf.c
+++ _printf.c
@@ -1,48 +1,68 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "main.h"
+/**
+ * print_formatted_string - takes in the format string and argument list
+ * @format: the format string
+ * @args: the argument list
+ * Return: the number of characters printed
+ */
+int print_formatted_string(const char *format, va_list args)
+{
+	int count = 0;
+	char *s;
 
-int print_formatted_string(const char* format, va_list args) {
-    int count = 0;
-    while (*format) {
-        if (*format == '%') {
-            switch (*(++format)) {
-                case 'c':
-                    _putchar((char)va_arg(args, int));
-                    count++;
-                    break;
-                case 's':
-                    char* s = va_arg(args, char*);
-                    for (; *s != '\0'; s++) {
-                        _putchar(*s);
-                        count++;
-                    break;
-                case '%':
-                    _putchar('%');
-                    count++;
-                    break;
-                default:
-                    _putchar('%');
-                    _putchar(*format);
-                    count += 2;
-                    break;
-            }
-        }
-        else {
-            putchar(*format);
-            count++;
-        }
-        format++;
-    }
-    return count;
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			switch (*(++format))
+			{
+				case 'c':
+					_putchar((char)va_arg(args, int));
+					count++;
+					break;
+				case 's':
+					s = va_arg(args, char*);
+					while (*s)
+					{
+						_putchar(*s);
+						s++;
+						count++;
+					}
+					break;
+				case '%':
+					_putchar('%');
+					count++;
+					break;
+				default:
+					_putchar('%');
+					_putchar(*format);
+					count += 2;
+					break;
+			}
+		}
+		else
+		{
+			_putchar(*format);
+			count++;
+		}
+		format++;
+	}
+	return (count);
 }
+/**
+ * _printf - produces output according to a format
+ * @format: the sring format
+ * Return: the number of characters printed
+ */
 int _printf(const char *format, ...)
 {
-    int count = 0;
-    va_list args;
-    va_start(args, format);
-    
-    count = print_formatted_string(format, args);
-    va_end(args);
-    return count;
+	int count = 0;
+	va_list args;
+
+	va_start(args, format);
+	count = print_formatted_string(format, args);
+	va_end(args);
+	return (count);
 }
