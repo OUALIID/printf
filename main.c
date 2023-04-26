3687f5fdf77650b7ac85e584c8d17302741bb920
OUALIID <walidelhadim8@gmail.com>
2023-04-26 02:58:41 +0100

    bainary

--- main.c
+++ main.c
@@ -40,5 +40,8 @@ int main(void)
     _printf("Unknown:[%r]\n");
     printf("Unknown:[%r]\n");
     _printf("%b\n", 98);
+    len_1 = _printf("%b\n", 98);
+    len_2 = printf("%b\n", 98);
+    printf("%d  ,   %d",len_1,len_2);
     return (0);
 }
